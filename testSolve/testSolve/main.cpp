22fb0240cdb42f7569b4511dd799b9c897306195
G Hester <gerard.hester@gmail.com>
2020-05-03 17:10:38 +0100

    Initial C++

---
+++ testSolve/testSolve/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testSolve
+//
+//  Created by Gerard Hester on 03/05/2020.
+//  Copyright © 2020 Gerard Hester. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
