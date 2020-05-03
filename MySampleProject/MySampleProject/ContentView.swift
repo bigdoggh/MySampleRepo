7494e1073b6aeba3e097078c01cee2ff232f96a9
G Hester <gerard.hester@gmail.com>
2020-05-03 17:02:00 +0100

    Initial project commit

---
+++ MySampleProject/MySampleProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySampleProject
+//
+//  Created by Gerard Hester on 03/05/2020.
+//  Copyright © 2020 Gerard Hester. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
